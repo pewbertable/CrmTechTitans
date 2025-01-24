6ae3ad819599dbc82ad52ebb85d3dcf4bab4b6ed
Ankirat Kaur <kaurankirat7@gmail.com>
2025-01-23 22:07:33 -0500

    added Industry table

---
+++ CrmTechTitans/Controllers/IndustryController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CrmTechTitans.Data;
+using CrmTechTitans.Models;
+
+namespace CrmTechTitans.Controllers
+{
+    public class IndustryController : Controller
+    {
+        private readonly CrmContext _context;
+
+        public IndustryController(CrmContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Industry
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Industries.Include(m => m.IndustryMembers).ThenInclude(im => im.Member).ToListAsync());
+        }
+
+        // GET: Industry/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var industry = await _context.Industries
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (industry == null)
+            {
+                return NotFound();
+            }
+
+            return View(industry);
+        }
+
+        // GET: Industry/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Industry/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,NAICS")] Industry industry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(industry);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(industry);
+        }
+
+        // GET: Industry/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var industry = await _context.Industries.FindAsync(id);
+            if (industry == null)
+            {
+                return NotFound();
+            }
+            return View(industry);
+        }
+
+        // POST: Industry/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,NAICS")] Industry industry)
+        {
+            if (id != industry.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(industry);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IndustryExists(industry.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(industry);
+        }
+
+        // GET: Industry/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var industry = await _context.Industries
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (industry == null)
+            {
+                return NotFound();
+            }
+
+            return View(industry);
+        }
+
+        // POST: Industry/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var industry = await _context.Industries.FindAsync(id);
+            if (industry != null)
+            {
+                _context.Industries.Remove(industry);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IndustryExists(int id)
+        {
+            return _context.Industries.Any(e => e.ID == id);
+        }
+    }
+}
