3c929418d3cba06d47db5aeb57d1fb586e569ff5
Braydon Pew <braydonp@live.ca>
2025-02-27 22:53:07 -0500

    Merged @jazz pagination

--- CrmTechTitans/Data/CrmInitializer.cs
+++ CrmTechTitans/Data/CrmInitializer.cs
@@ -92,13 +92,15 @@ namespace CrmTechTitans.Data
                     if (!context.MembershipTypes.Any())
                     {
                         context.MembershipTypes.AddRange(
+
                             new MembershipType { ID = 1, Name = "Associate" },
-                            new MembershipType { ID = 2, Name = "Local Industrial" },
-                            new MembershipType { ID = 3, Name = "Chamber Associate" },
-                            new MembershipType { ID = 4, Name = "Government Education Associate" },
-                            new MembershipType { ID = 5, Name = "Non-Local Industrial" },
-                            new MembershipType { ID = 6, Name = "Other" }
-                        );
+                            new MembershipType { ID = 2, Name = "Local" },
+                            new MembershipType { ID = 3, Name = "Chamber" },
+                            new MembershipType { ID = 4, Name = "Government Education" },
+                            new MembershipType { ID = 5, Name = "Non-Local" },
+                            new MembershipType { ID = 6, Name = "Industrial" },
+                            new MembershipType { ID = 7, Name = "Other" }
+                            );
                         context.SaveChanges();
                     }
 
