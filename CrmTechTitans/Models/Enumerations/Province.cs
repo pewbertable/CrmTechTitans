48244dcae8629294dae5cd52815c058f6686260c
Ankirat Kaur <kaurankirat7@gmail.com>
2025-03-30 18:47:29 -0400

    fixed opp & munci.

--- CrmTechTitans/Models/Enumerations/Province.cs
+++ CrmTechTitans/Models/Enumerations/Province.cs
@@ -1,14 +1,21 @@
-﻿namespace CrmTechTitans.Models.Enumerations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrmTechTitans.Models.Enumerations
 {
     public enum Province
     {
         Alberta,
+        [Display(Name = "British Columbia")]
         BritishColumbia,
         Manitoba,
+        [Display(Name = "New Brunswick")]
         NewBrunswick,
+        [Display(Name = "Newfoundland And Labrador")]
         NewfoundlandAndLabrador,
+        [Display(Name = "Nova Scotia")]
         NovaScotia,
         Ontario,
+        [Display(Name = "Prince Edward Island")]
         PrinceEdwardIsland,
         Quebec,
         Saskatchewan
